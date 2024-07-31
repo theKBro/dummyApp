dd7d0f42f4c0d99918d3541299f1d968ece76983
theKBro <K.Broselge@gmail.com>
2024-07-31 20:24:23 +0200

    initial dummy app

---
+++ dummyApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using dummyDependency;
+
+Console.WriteLine("Hello, World!");
+var dummyDependency = new dummyDep();
+dummyDependency.dummyMethod();
